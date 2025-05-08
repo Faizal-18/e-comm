b9193c03e69c81f6e177ad110f85d02ae43b46c8
Faizal-18 <faizalmd1607@gmail.com>
2025-05-08 12:50:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-// Navbar toggle
-const bar = document.getElementById('bar');
-const close = document.getElementById('close');
-const nav = document.getElementById('navbar');
+document.addEventListener("DOMContentLoaded", () => {
+  const bar = document.getElementById('bar');
+  const close = document.getElementById('close');
+  const nav = document.getElementById('navbar');
 
-if (bar) {
-  bar.addEventListener('click', () => nav.classList.add('active'));
-  if (close) close.addEventListener('click', () => nav.classList.remove('active'));
-}
+  if (bar) {
+    bar.addEventListener('click', () => nav.classList.add('active'));
+  }
+
+  if (close) {
+    close.addEventListener('click', () => nav.classList.remove('active'));
+  }
+});
+
+
 
 // Carousel image switch
 document.addEventListener("DOMContentLoaded", function () {
